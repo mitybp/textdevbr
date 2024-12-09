bf55c1b159e5c660285f4b43fd624eab1b78f8fd
mitybp <dimitribpusch21@gmail.com>
2024-12-09 13:15:18 -0300

    add vercel env

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANGza3ijLrrXDHds2g-mljk4NR5Pf32PQ",
-  authDomain: "textdevbr-auth.firebaseapp.com",
-  projectId: "textdevbr-auth",
-  storageBucket: "textdevbr-auth.firebasestorage.app",
-  messagingSenderId: "639840622441",
-  appId: "1:639840622441:web:9742ebfb3e839d8f6a4ff8"
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
